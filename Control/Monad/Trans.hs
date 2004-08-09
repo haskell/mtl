3cb438564b2b2a9544c6367e98e7870487d5b442
Simon Marlow <marlowsd@gmail.com>
2004-08-09 11:59:48 +0000

    whitespace changes to the header only, for new compatibility with new Haddock

--- Control/Monad/Trans.hs
+++ Control/Monad/Trans.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module      :  Control.Monad.Trans
 -- Copyright   :  (c) Andy Gill 2001,
---		  (c) Oregon Graduate Institute of Science and Technology, 2001
+-- 		  (c) Oregon Graduate Institute of Science and Technology, 2001
 -- License     :  BSD-style (see the file libraries/base/LICENSE)
 -- 
 -- Maintainer  :  libraries@haskell.org
