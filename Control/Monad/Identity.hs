0c671e436828eca106ec3eefd3c34e3e9057f172
Andriy Palamarchuk <apa3a@yahoo.com>
2006-12-18 16:56:21 +0000

    Added Haddock documentation. Converted the module documentation to Haddock format. Per Jeff Newbern's permission included parts his tutorial "All About Monads" http://www.nomaware.com/monads/.

--- Control/Monad/Identity.hs
+++ Control/Monad/Identity.hs
@@ -1,26 +1,46 @@
------------------------------------------------------------------------------
--- |
--- Module      :  Control.Monad.Identity
--- Copyright   :  (c) Andy Gill 2001,
--- 		  (c) Oregon Graduate Institute of Science and Technology, 2001
--- License     :  BSD-style (see the file libraries/base/LICENSE)
--- 
--- Maintainer  :  libraries@haskell.org
--- Stability   :  experimental
--- Portability :  portable
---
--- The Identity monad.
---
---	  Inspired by the paper
---	  /Functional Programming with Overloading and
---	      Higher-Order Polymorphism/, 
---	    Mark P Jones (<http://www.cse.ogi.edu/~mpj/>)
---		  Advanced School of Functional Programming, 1995.
---
------------------------------------------------------------------------------
+{- |
+Module      :  Control.Monad.Identity
+Copyright   :  (c) Andy Gill 2001,
+      (c) Oregon Graduate Institute of Science and Technology 2001,
+      (c) Jeff Newbern 2003-2006,
+      (c) Andriy Palamarchuk 2006
+License     :  BSD-style (see the file libraries/base/LICENSE)
+
+Maintainer  :  libraries@haskell.org
+Stability   :  experimental
+Portability :  portable
+
+[Computation type:] Simple function application.
+
+[Binding strategy:] The bound function is applied to the input value.
+@'Identity' x >>= f == 'Identity' (f x)@
+
+[Useful for:] Monads can be derived from monad transformers applied to the
+'Identity' monad.
+
+[Zero and plus:] None.
+
+[Example type:] @'Identity' a@
+
+The @Identity@ monad is a monad that does not embody any computational strategy.
+It simply applies the bound function to its input without any modification.
+Computationally, there is no reason to use the @Identity@ monad
+instead of the much simpler act of simply applying functions to their arguments.
+The purpose of the @Identity@ monad is its fundamental role in the theory
+of monad transformers.
+Any monad transformer applied to the @Identity@ monad yields a non-transformer
+version of that monad.
+
+  Inspired by the paper
+  /Functional Programming with Overloading and
+      Higher-Order Polymorphism/, 
+    Mark P Jones (<http://www.cse.ogi.edu/~mpj/>)
+	  Advanced School of Functional Programming, 1995.
+-}
 
 module Control.Monad.Identity (
 	Identity(..),
+
 	module Control.Monad,
 	module Control.Monad.Fix,
    ) where
@@ -30,19 +50,36 @@ import Prelude
 import Control.Monad
 import Control.Monad.Fix
 
--- ---------------------------------------------------------------------------
--- Identity wrapper
---
---	Abstraction for wrapping up a object.
---	If you have an monadic function, say:
---
---	    example :: Int -> IdentityMonad Int
---	    example x = return (x*x)
---
---      you can "run" it, using
---
---	  Main> runIdentity (example 42)
---	  1764 :: Int
+{- | Identity wrapper.
+Abstraction for wrapping up a object.
+If you have an monadic function, say:
+  
+>   example :: Int -> Identity Int
+>   example x = return (x*x)
+  
+     you can \"run\" it, using
+
+> Main> runIdentity (example 42)
+> 1764 :: Int
+
+A typical use of the Identity monad is to derive a monad
+from a monad transformer.
+
+@
+-- derive the 'Control.Monad.State.State' monad using the 'Control.Monad.State.StateT' monad transformer
+type 'Control.Monad.State.State' s a = 'Control.Monad.State.StateT' s 'Identity' a
+@
+
+The @'runIdentity'@ label is used in the type definition because it follows
+a style of monad definition that explicitly represents monad values as
+computations. In this style, a monadic computation is built up using the monadic
+operators and then the value of the computation is extracted
+using the @run******@ function.
+Because the @Identity@ monad does not do any computation, its definition
+is trivial.
+For a better example of this style of monad,
+see the @'Control.Monad.State.State'@ monad.
+-}
 
 newtype Identity a = Identity { runIdentity :: a }
 
