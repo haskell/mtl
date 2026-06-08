120e91dcd5f2e67cf927dcbef22f99bf03cf725e
Steven Shuck <stevenjshuck@gmail.com>
2026-06-08 05:27:44 -0400

    Remove CPP for ContT MonadCont instance; update tested-with
    The kind signature does not work in MHS, and it isn't necessary in GHC,
    so delete it. Test for the latter.

--- Control/Monad/Cont/Class.hs
+++ Control/Monad/Cont/Class.hs
@@ -114,12 +114,7 @@ class Monad m => MonadCont (m :: Type -> Type) where
     {-# MINIMAL callCC #-}
 
 -- | @since 2.3.1
-#ifdef __MHS__
--- The ContT type is not polykinded with mhs.
 instance MonadCont (ContT r m) where
-#else
-instance forall k (r :: k) (m :: (k -> Type)) . MonadCont (ContT r m) where
-#endif
     callCC = ContT.callCC
 
 -- ---------------------------------------------------------------------------
